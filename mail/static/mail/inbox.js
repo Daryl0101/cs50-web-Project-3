d4a3eb0b514c88a6f90e6da40d4841128d90e2f5
Daryl Tneoh <daryl.tneoh@gmail.com>
2021-09-25 02:09:35 +0800

    Fourth commit: View Email done

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -41,6 +41,7 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#mails-list').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#clickedMail-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -54,6 +55,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#mails-list').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#clickedMail-view').style.display = 'none';
 
   // Clear previous mail items
   document.querySelectorAll('.mail-read').forEach(mail => mail.remove());
@@ -90,7 +92,64 @@ function add_email(mail) {
   <div class="mail-time">${mail.timestamp}</div>
   </div>
   <div class="mail-subNbody"><b>${mail.subject}</b> - ${mail.body}</div>`;
+  item.addEventListener('click', function(){
+    console.log('This element has been clicked!');
+    fetch(`/emails/${mail.id}`)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      load_clickedMail(result);
+    });
+    fetch(`/emails/${mail.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        read: true
+      })
+    });
+  });
 
   // Add mail to DOM
   document.querySelector('#mails-list').append(item);
+}
+
+function load_clickedMail(mail) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#mails-list').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#clickedMail-view').style.display = 'block';
+  
+  // Clear previous sender and recipient items
+  if(document.querySelector('.clickedMail-content')){
+    document.querySelector('.clickedMail-content').remove();
+  }
+
+  // Content of clicked email
+  const item = document.createElement('div');
+  item.className = 'clickedMail-content';
+  item.innerHTML = 
+  `<h3 id="clickedMail-subject">${mail.subject}</h3>
+  <br>
+  <div id="clickedMail-time">${mail.timestamp}</div>
+  <hr>
+  <div id="clickedMail-sender"><b>Sender:</b> <i>${mail.sender}</i></div>
+  <div id="clickedMail-recipients"><b>Recipients:</b> </div>
+  <br>
+  <div id="clickedMail-body">${mail.body}</div>`
+  document.querySelector('#clickedMail-view').append(item);
+
+  // Loop for each recipients
+  var counter = 1;
+  mail.recipients.forEach(recipient => {
+    const rcp = document.createElement('span');
+    if(counter==mail.recipients.length) {
+      rcp.innerHTML = `<i>${recipient}</i>`;
+    }
+    else {
+      rcp.innerHTML = `<i>${recipient}, </i>`;
+    }
+    document.querySelector('#clickedMail-recipients').append(rcp);
+    counter+=1;
+  });
 }
