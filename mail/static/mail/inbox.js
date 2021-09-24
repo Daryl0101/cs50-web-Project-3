f484079dad33c8d0ed54cb455cd278050d81c7f5
Daryl Tneoh <daryl.tneoh@gmail.com>
2021-09-24 22:30:54 +0800

    Third commit: Mailbox done

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // By default, load the inbox
   load_mailbox('inbox');
 
+  // Submit the form if recipient is valid and show success message, otherwise load an empty form and show error message
   document.querySelector('#compose-form').onsubmit = function() {
     fetch('/emails', {
       method: 'POST',
@@ -38,6 +39,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#mails-list').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -50,8 +52,45 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#mails-list').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+
+  // Clear previous mail items
+  document.querySelectorAll('.mail-read').forEach(mail => mail.remove());
+  document.querySelectorAll('.mail-unread').forEach(mail => mail.remove());
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Load the appropriate mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+    emails.forEach(add_email);
+  });
+}
+
+// Add a new mail with given contents to DOM
+function add_email(mail) {
+  // Create new mail
+  const item = document.createElement('div');
+  
+  // Mark read and unread mails with different classes
+  if(mail.read===false) {
+    item.className = 'mail-unread';
+  }
+  else {
+    item.className = 'mail-read';
+  }
+
+  item.innerHTML = 
+  `<div class="mail-content">
+  <div class="mail-sender">${mail.sender}</div>
+  <div class="mail-time">${mail.timestamp}</div>
+  </div>
+  <div class="mail-subNbody"><b>${mail.subject}</b> - ${mail.body}</div>`;
+
+  // Add mail to DOM
+  document.querySelector('#mails-list').append(item);
 }
